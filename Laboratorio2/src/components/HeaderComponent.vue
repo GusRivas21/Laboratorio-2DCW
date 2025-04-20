5d0f5cbf304152214ade95f43ac325dfd064397e
GusRivas21 <gustavo.rivas1270@gmail.com>
2025-04-20 12:27:17 -0600

    pequeños arreglos de codigo

--- Laboratorio2/src/components/HeaderComponent.vue
+++ Laboratorio2/src/components/HeaderComponent.vue
@@ -1,3 +1,13 @@
+    <script setup>
+        import { defineEmits } from 'vue';
+
+        const emit = defineEmits(['cambiar-seccion']);
+
+        const cambiarSeccion = (seccion) => {
+            emit('cambiar-seccion', seccion);
+        };
+    </script>
+
     <template>
         <nav class="navbar">
         <div class="logo-container">
@@ -11,54 +21,44 @@
         </nav>
     </template>
 
-    <script setup>
-    import { defineEmits } from 'vue';
-
-    const emit = defineEmits(['cambiar-seccion']);
-
-    const cambiarSeccion = (seccion) => {
-        emit('cambiar-seccion', seccion);
-    };
-    </script>
-
     <style scoped>
-    .navbar {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        background-color: rgb(6, 6, 6);
-        padding: 15px 20px;
-        color: white;
-        width: 100%;
-        box-sizing: border-box;
-    }
+        .navbar {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            background-color: rgb(6, 6, 6);
+            padding: 15px 20px;
+            color: white;
+            width: 100%;
+            box-sizing: border-box;
+        }
 
-    .logo-container {
-        display: flex;
-        align-items: center;
-        gap: 10px;
-    }
+        .logo-container {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
 
-    .logo {
-        width: 70px; /* Ajusta el tamaño según necesites */
-        height: auto;
-    }
+        .logo {
+            width: 70px;
+            height: auto;
+        }
 
-    .navbar ul {
-        list-style: none;
-        display: flex;
-        gap: 20px;
-        padding: 0;
-        margin: 0;
-    }
+        .navbar ul {
+            list-style: none;
+            display: flex;
+            gap: 20px;
+            padding: 0;
+            margin: 0;
+        }
 
-    .navbar a {
-        text-decoration: none;
-        color: white;
-        font-weight: bold;
-    }
+        .navbar a {
+            text-decoration: none;
+            color: white;
+            font-weight: bold;
+        }
 
-    .navbar a:hover {
-        text-decoration: underline;
-    }
+        .navbar a:hover {
+            text-decoration: underline;
+        }
     </style>
